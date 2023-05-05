89b79edc9dd4570707d7e02fce6d0116e21eac52
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-05 17:10:21 +0600

    Add 404 error handler

---
+++ AVN/Controllers/ErrorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AVN.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/NotFound/{statusCode}")]
+        public IActionResult NotFound(int statusCode) => View();
+    }
+}
