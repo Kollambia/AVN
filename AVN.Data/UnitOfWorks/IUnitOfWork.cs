172fb2b35a6f44de86b5f5d1708ac8d86ed4f1a7
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-12 11:14:57 +0600

    Addendum to the previous commit

--- AVN.Data/UnitOfWorks/IUnitOfWork.cs
+++ AVN.Data/UnitOfWorks/IUnitOfWork.cs
@@ -7,5 +7,7 @@ public interface IUnitOfWork : IDisposable
 {
     IRepository<Student> StudentRepository { get; }
     IRepository<Faculty> FacultyRepository { get;  }
+    IRepository<Department> DepartmentRepository { get;  }
+    IRepository<Group> GroupRepository { get; }
     Task<int> SaveChangesAsync();
 }
