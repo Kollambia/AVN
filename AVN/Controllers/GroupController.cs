f6582eb947c502cd70e5728986b2087e7ffeb22d
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-17 21:40:36 +0600

    Add GroupController without View

---
+++ AVN/Controllers/GroupController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AVN.Data.UnitOfWorks;
+using AVN.Model.Entities;
+using System.Threading.Tasks;
+
+namespace AVN.Web.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GroupController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Group
+        public async Task<IActionResult> Index()
+        {
+            return View(await _unitOfWork.GroupRepository.GetAllAsync());
+        }
+
+        // GET: Group/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var group = await _unitOfWork.GroupRepository.GetByIdAsync(id);
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return View(group);
+        }
+
+        // GET: Group/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Group/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,GroupName,Course,DateCreate,DirectionId")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.GroupRepository.CreateAsync(group);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(group);
+        }
+
+        // GET: Group/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var group = await _unitOfWork.GroupRepository.GetByIdAsync(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Group/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GroupName,Course,DateCreate,DirectionId")] Group group)
+        {
+            if (id != group.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.GroupRepository.UpdateAsync(group);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(group);
+        }
+
+        // GET: Group/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var group = await _unitOfWork.GroupRepository.GetByIdAsync(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return View(group);
+        }
+
+        // POST: Group/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var group = await _unitOfWork.GroupRepository.GetByIdAsync(id);
+            await _unitOfWork.GroupRepository.DeleteAsync(group);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
