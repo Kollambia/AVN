c799d5e8f50145bd1e0275d772b98fcad2dc9ff3
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-17 21:03:26 +0600

    Fix Migration Error and Add FacultyController

---
+++ AVN.Data/Migrations/20230517145856_AddStudentPaymentsAndStudentPaymentsDetails.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AVN.Data.Migrations
+{
+    public partial class AddStudentPaymentsAndStudentPaymentsDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "StudingForm",
+                table: "Students",
+                type: "int",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Students",
+                type: "int",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EducationalLine",
+                table: "Students",
+                type: "int",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AcademicDegree",
+                table: "Students",
+                type: "int",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Course",
+                table: "Groups",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreate",
+                table: "Groups",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DirectionShortName",
+                table: "Directions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Directions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CreditCost",
+                table: "Directions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "StudentPaymentDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Payment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentType = table.Column<int>(type: "int", nullable: false),
+                    SpecialPurpose = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentPaymentDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentPayments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AcademicYear = table.Column<int>(type: "int", nullable: false),
+                    Contract = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Payed = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Debt = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentPayments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentPayments_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreditHours = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Course = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subjects_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectEmployees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectId = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    EmployeeCreditHours = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectEmployees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubjectEmployees_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_SubjectEmployees_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentPayments_StudentId",
+                table: "StudentPayments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectEmployees_EmployeeId",
+                table: "SubjectEmployees",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectEmployees_SubjectId",
+                table: "SubjectEmployees",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_DepartmentId",
+                table: "Subjects",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentPaymentDetails");
+
+            migrationBuilder.DropTable(
+                name: "StudentPayments");
+
+            migrationBuilder.DropTable(
+                name: "SubjectEmployees");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "AcademicDegree",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreate",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "CreditCost",
+                table: "Directions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudingForm",
+                table: "Students",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Students",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Students",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EducationalLine",
+                table: "Students",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DirectionShortName",
+                table: "Directions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Directions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- AVN/Controllers/FacultyController.cs
+++ AVN/Controllers/FacultyController.cs
@@ -1,23 +1,109 @@
-﻿using AVN.Data.UnitOfWorks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using AVN.Data.UnitOfWorks;
+using AVN.Model.Entities;
+using System.Threading.Tasks;
 
-namespace AVN.Controllers;
-
-public class FacultyController : Controller
+namespace AVN.Web.Controllers
 {
-    private readonly IUnitOfWork _unitOfWork;
-
-    public FacultyController(IUnitOfWork unitOfWork)
+    public class FacultyController : Controller
     {
-        _unitOfWork = unitOfWork;
-    }
-    
-    [HttpGet]
-    public async Task<IActionResult> GetFaculties()
-    {
-        //var faculty = await _unitOfWork.FacultyRepository.GetAll();
-        return View();
-    }
+        private readonly IUnitOfWork _unitOfWork;
 
-   
+        public FacultyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Faculty
+        public async Task<IActionResult> Index()
+        {
+            return View(await _unitOfWork.FacultyRepository.GetAllAsync());
+        }
+
+        // GET: Faculty/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var faculty = await _unitOfWork.FacultyRepository.GetByIdAsync(id);
+
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Faculty/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Faculty/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FacultyName,FacultyShortName,DeanName")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.FacultyRepository.CreateAsync(faculty);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculty/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var faculty = await _unitOfWork.FacultyRepository.GetByIdAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Faculty/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FacultyName,FacultyShortName,DeanName")] Faculty faculty)
+        {
+            if (id != faculty.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.FacultyRepository.UpdateAsync(faculty);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculty/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var faculty = await _unitOfWork.FacultyRepository.GetByIdAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // POST: Faculty/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var faculty = await _unitOfWork.FacultyRepository.GetByIdAsync(id);
+            await _unitOfWork.FacultyRepository.DeleteAsync(faculty);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
