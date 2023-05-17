565e6e2ec18147ea4071c9012d9b9b9871b7afac
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-17 20:17:17 +0600

    Add StudentController without View

--- AVN/Controllers/StudentController.cs
+++ AVN/Controllers/StudentController.cs
@@ -1,24 +1,112 @@
-﻿using System.Collections;
+﻿using Microsoft.AspNetCore.Mvc;
 using AVN.Data.UnitOfWorks;
 using AVN.Model.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
-namespace AVN.Controllers;
-
-public class StudentController : Controller
+namespace AVN.Web.Controllers
 {
-    private readonly IUnitOfWork _unitOfWork;
-
-    public StudentController(IUnitOfWork unitOfWork)
+    public class StudentController : Controller
     {
-        _unitOfWork = unitOfWork;
-    }
+        private readonly IUnitOfWork _unitOfWork;
 
-    [HttpGet]
-    public async Task<IActionResult> GetStudents()
-    {
-        var students = _unitOfWork.StudentRepository.GetAllAsync();
-        return View();
+        public StudentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Student
+        public async Task<IActionResult> Index()
+        {
+            return View(await _unitOfWork.StudentRepository.GetAllAsync());
+        }
+
+        // GET: Student/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var student = await _unitOfWork.StudentRepository.GetByIdAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FullName,Status,DateOfBirth,StudingForm," +
+                                                      "EducationalLine,AcademicDegree,GradeBookNumber," +
+                                                      "Gender,Citizenship,Address,PhoneNumber,Orders,GroupId")] 
+                                                        Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.StudentRepository.CreateAsync(student);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _unitOfWork.StudentRepository.GetByIdAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,Status,DateOfBirth,StudingForm,EducationalLine,AcademicDegree,GradeBookNumber,Gender,Citizenship,Address,PhoneNumber,Orders,GroupId")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.StudentRepository.UpdateAsync(student);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _unitOfWork.StudentRepository.GetByIdAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _unitOfWork.StudentRepository.GetByIdAsync(id);
+            await _unitOfWork.StudentRepository.DeleteAsync(student);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
