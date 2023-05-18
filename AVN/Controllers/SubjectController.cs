d4e18b111102633ebb752ce962f3d973752d5783
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-18 11:30:00 +0600

    Add SubjectContoller without View

---
+++ AVN/Controllers/SubjectController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AVN.Model.Entities;
+using AVN.Data.UnitOfWorks;
+
+namespace AVN.Web.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SubjectController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Subject
+        public async Task<IActionResult> Index()
+        {
+            return View(await _unitOfWork.SubjectRepository.GetAllAsync());
+        }
+
+        // GET: Subject/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var subject = await _unitOfWork.SubjectRepository.GetByIdAsync(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // GET: Subject/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subject/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.SubjectRepository.CreateAsync(subject);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(subject);
+        }
+
+        // GET: Subject/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var subject = await _unitOfWork.SubjectRepository.GetByIdAsync(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Subject/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [FromForm] Subject subject)
+        {
+            if (id != subject.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.SubjectRepository.UpdateAsync(subject);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(subject);
+        }
+
+        // GET: Subject/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var subject = await _unitOfWork.SubjectRepository.GetByIdAsync(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Subject/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subject = await _unitOfWork.SubjectRepository.GetByIdAsync(id);
+            await _unitOfWork.SubjectRepository.DeleteAsync(subject);
+            await _unitOfWork.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
