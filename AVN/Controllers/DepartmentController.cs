0e19f3ba1521f7a7c4505254d18d78817b981f38
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-18 10:51:01 +0600

    Add DepartmentController without view

---
+++ AVN/Controllers/DepartmentController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AVN.Data.UnitOfWorks;
+using AVN.Model.Entities;
+using System.Threading.Tasks;
+
+namespace AVN.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DepartmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Department
+        public async Task<IActionResult> Index()
+        {
+            return View(await _unitOfWork.DepartmentRepository.GetAllAsync());
+        }
+
+        // GET: Department/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var department = await _unitOfWork.DepartmentRepository.GetByIdAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DepartmentName,DepartmentShortName,FacultyId")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.DepartmentRepository.CreateAsync(department);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var department = await _unitOfWork.DepartmentRepository.GetByIdAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DepartmentName,DepartmentShortName,FacultyId")] Department department)
+        {
+            if (id != department.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.DepartmentRepository.UpdateAsync(department);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Department/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var department = await _unitOfWork.DepartmentRepository.GetByIdAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _unitOfWork.DepartmentRepository.GetByIdAsync(id);
+            await _unitOfWork.DepartmentRepository.DeleteAsync(department);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
