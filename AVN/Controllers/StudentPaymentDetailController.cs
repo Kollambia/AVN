87ef56b650ffa4078d987dbf3f2bdae433813451
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-18 21:32:12 +0600

    Add StudentPaymentDetails without view

---
+++ AVN/Controllers/StudentPaymentDetailController.cs
@@ -0,0 +1,93 @@
+﻿using AVN.Data.UnitOfWorks;
+using AVN.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class StudentPaymentDetailsController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public StudentPaymentDetailsController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _unitOfWork.StudentPaymentDetailRepository.GetAllAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Payment,PaymentDate,Number,PaymentType,SpecialPurpose,StudentPaymentId")] StudentPaymentDetail studentPaymentDetail)
+    {
+        if (ModelState.IsValid)
+        {
+            await _unitOfWork.StudentPaymentDetailRepository.CreateAsync(studentPaymentDetail);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(studentPaymentDetail);
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var studentPaymentDetail = await _unitOfWork.StudentPaymentDetailRepository.GetByIdAsync(id.Value);
+        if (studentPaymentDetail == null)
+        {
+            return NotFound();
+        }
+        return View(studentPaymentDetail);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Payment,PaymentDate,Number,PaymentType,SpecialPurpose,StudentPaymentId")] StudentPaymentDetail studentPaymentDetail)
+    {
+        if (id != studentPaymentDetail.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            await _unitOfWork.StudentPaymentDetailRepository.UpdateAsync(studentPaymentDetail);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(studentPaymentDetail);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var studentPaymentDetail = await _unitOfWork.StudentPaymentDetailRepository.GetByIdAsync(id.Value);
+        if (studentPaymentDetail == null)
+        {
+            return NotFound();
+        }
+
+        return View(studentPaymentDetail);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var studentPaymentDetail = await _unitOfWork.StudentPaymentDetailRepository.GetByIdAsync(id);
+        await _unitOfWork.StudentPaymentDetailRepository.DeleteAsync(studentPaymentDetail);
+        return RedirectToAction(nameof(Index));
+    }
+}
