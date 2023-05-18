767bbe62929876a84864985978c9af895e40f866
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-18 11:29:16 +0600

    Add EmployeeController without View

---
+++ AVN/Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AVN.Data.UnitOfWorks;
+using AVN.Model.Entities;
+
+namespace AVN.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EmployeeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: Employee
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _unitOfWork.EmployeeRepository.GetAllAsync();
+            return View(employees);
+        }
+
+        // GET: Employee/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var employee = await _unitOfWork.EmployeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.EmployeeRepository.CreateAsync(employee);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var employee = await _unitOfWork.EmployeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.EmployeeRepository.UpdateAsync(employee);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employee = await _unitOfWork.EmployeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _unitOfWork.EmployeeRepository.GetByIdAsync(id);
+            await _unitOfWork.EmployeeRepository.DeleteAsync(employee);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
