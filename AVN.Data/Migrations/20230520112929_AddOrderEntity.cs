c6ca382699122fccdc910105669d5562a3ea6fed
Miristan Gadaibaev <miristan.kg@mail.ru>
2023-05-26 23:27:36 +0600

    fix migration

--- AVN.Data/Migrations/20230520112929_AddOrderEntity.cs
+++ AVN.Data/Migrations/20230520112929_AddOrderEntity.cs
@@ -9,37 +9,6 @@ namespace AVN.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Orders",
-                table: "Students");
-
-            migrationBuilder.CreateTable(
-                name: "Order",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    OrderType = table.Column<int>(type: "int", nullable: false),
-                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    StudentId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Order", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Order_Students_StudentId",
-                        column: x => x.StudentId,
-                        principalTable: "Students",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Order_StudentId",
-                table: "Order",
-                column: "StudentId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
