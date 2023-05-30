f2d2c54677d6ff4312775239902b070e38e39bc9
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-30 16:12:12 +0600

    Add new fields to appuser

--- AVN.Model/Entities/AppUser.cs
+++ AVN.Model/Entities/AppUser.cs
@@ -5,6 +5,40 @@ namespace AVN.Model.Entities
 {
     public class AppUser : IdentityUser
     {
+        public string? SName { get; set; }
 
+        public string? Name { get; set; }
+
+        public string? PName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public FormOfEducation? StudingForm { get; set; }
+
+        public EducationalLine? EducationalLine { get; set; }
+
+        public AcademicDegree? AcademicDegree { get; set; }
+
+        public string? GradeBookNumber { get; set; }
+
+        public StudentStatus? Status { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public Citizenship? Citizenship { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public int? FacultyId { get; set; }
+
+        public int? DepartmentId { get; set; }
+
+        public int? DirectionId { get; set; }
+
+        public int? GroupId { get; set; }
+
+        public virtual Group? Group { get; set; }
     }
 }
