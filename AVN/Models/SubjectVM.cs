a2a209164203a8f12aea97148654cc15c4726f35
Ermek Abilov <ermek.abilove@gmail.com>
2023-06-03 22:18:58 +0600

    Create SubjectVM, Add subject Mapper, Create Subject index and Create subject Page

---
+++ AVN/Models/SubjectVM.cs
@@ -0,0 +1,34 @@
+﻿using AVN.Common.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using AVN.Model.Entities;
+
+namespace AVN.Models
+{
+    public class SubjectVM : BasicVM<int>
+    {
+        [Required(ErrorMessage = "Введите название предмета")]
+        [DisplayName("Название")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Поле должно содержать от 3 до 100 символов.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Введите описание")]
+        [DisplayName("Описание")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Поле должно содержать от 3 до 100 символов.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Введите описание")]
+        [DisplayName("Количество часов")]
+        [StringLength(150, MinimumLength = 1, ErrorMessage = "Поле должно содержать минимум 1 символ")]
+        public string CreditHours { get; set; }
+
+        [Required(ErrorMessage = "Выберите номер курса")]
+        [DisplayName("Курс")]
+        public Course Course { get; set; }
+
+        [Required(ErrorMessage = "Выберите преподавателя для предмета")]
+        [DisplayName("Преподаватели")]
+        public List<int> EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
