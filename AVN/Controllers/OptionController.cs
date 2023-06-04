1ce13b4f4ef36468ff7279ea238fa63ebfe29cf4
miristan gadaibaev <miristan.kg@gmail.com>
2023-06-05 01:13:49 +0600

    create option controller

--- AVN/Controllers/OptionController.cs
+++ AVN/Controllers/OptionController.cs
@@ -5,27 +5,31 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AVN.Controllers
 {
-    public class AcademicYearController : Controller
+    public class OptionController : Controller
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
-
-        public AcademicYearController(IUnitOfWork unitOfWork, IMapper mapper)
+        public OptionController(IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
         }
-    
+        
         public IActionResult Index()
         {
             return View();
         }
+
         public async Task<List<SelectListItem>> GetAcademicYears()
         {
             var academicYears = (await unitOfWork.AcademicYearRepository.GetAllAsync()).OrderByDescending(x => x.Id);
-            //var academicYears = await unitOfWork.AcademicYearRepository.GetAllAsync();
-            var academYearList = academicYears.Select(f => new SelectListItem { Value = f.Id.ToString(), Text = f.Name }).ToList();
-            return academYearList;
+            return academicYears.Select(f => new SelectListItem { Value = f.Id.ToString(), Text = f.Name }).ToList();
+        }
+
+        public async Task<List<SelectListItem>> GetMovementTypes()
+        {
+            var movementTypes = await unitOfWork.MovementTypeRepository.GetAllAsync();
+            return movementTypes.Select(f => new SelectListItem { Value = f.Id.ToString(), Text = f.Name }).ToList();
         }
     }
 }
