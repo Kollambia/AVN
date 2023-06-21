606e05990cd40105e38b55c67619d6d720769ba4
Ermek Abilov <ermek.abilove@gmail.com>
2023-06-21 13:17:08 +0600

    New line in grades enum

--- AVN.Common/Enums/Grades.cs
+++ AVN.Common/Enums/Grades.cs
@@ -4,12 +4,12 @@ namespace AVN.Common.Enums
 {
     public enum Grades
     {
+        [Display(Name = "н/я")] DontPass = 0,
         [Display(Name = "Отл.")] Great = 1,
         [Display(Name = "Хор.")] Good = 2,
         [Display(Name = "Удов.")] Satisfactorily = 3,
         [Display(Name = "Неудов.")] Unsatisfactory = 4,
         [Display(Name = "Зачет")] Offset = 5,
         [Display(Name = "Незачет")] Fail = 6
-
     }
 }
