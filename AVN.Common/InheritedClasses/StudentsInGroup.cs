f509aac8f2bc8a22351423835cea614690c6de78
miristan gadaibaev <miristan.kg@gmail.com>
2023-06-30 23:28:10 +0600

    add reportController, create new report

---
+++ AVN.Common/InheritedClasses/StudentsInGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace AVN.Common.InheritedClasses
+{
+    public class StudentsInGroup
+    {
+        public string? FullName { get; set; }
+        public string? Status { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? EducationalLine { get; set; }
+        public string? GradeBookNumber { get; set; }
+        public string? Gender { get; set; }
+        public string? Citizenship { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? RecruitmentYear { get; set; }
+        public string? IsHasDebt { get; set; }
+        public string? Group { get; set; }
+        public string? Direction { get; set; }
+    }
+}
